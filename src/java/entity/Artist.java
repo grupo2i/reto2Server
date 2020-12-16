08f57823db8a1bbf0b18f7117f05c63ebde331e0
grupo2i <72964155+grupo2i@users.noreply.github.com>
2020-12-16 11:35:23 +0100

    Update Artist.java

--- src/java/entity/Artist.java
+++ src/java/entity/Artist.java
@@ -69,12 +69,12 @@ public class Artist extends User implements Serializable {
         if (!(object instanceof Artist)) {
             return false;
         }
-        Artist other = (Artist) object;
-         return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
+       Artist other = (Artist) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
         }
         return true;
     }
-
     @Override
     public String toString() {
         return "entity.artista[ id=" + id + " ]";
