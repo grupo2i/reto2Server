58c9e228f21be00a17f6bae2b5ecf903588b9952
grupo2i <72964155+grupo2i@users.noreply.github.com>
2020-12-16 11:38:07 +0100

    Update User.java

--- src/java/entity/User.java
+++ src/java/entity/User.java
@@ -202,10 +202,13 @@ public class User implements Serializable {
         if (!(object instanceof User)) {
             return false;
         }
-        User other = (User) object;
-        return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
+         User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
     }
-
+    
     @Override
     public String toString() {
         return "entity.User[ id=" + id + " ]";
