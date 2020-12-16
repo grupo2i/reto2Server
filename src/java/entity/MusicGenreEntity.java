cf433a7613caaf048f1056f0ecafd4b350444add
grupo2i <72964155+grupo2i@users.noreply.github.com>
2020-12-16 11:42:03 +0100

    Update MusicGenreEntity.java

--- src/java/entity/MusicGenreEntity.java
+++ src/java/entity/MusicGenreEntity.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
