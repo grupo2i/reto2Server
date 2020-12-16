4a60ef70019c1893ef37d2ad109ae2f2689c50e7
grupo2i <72964155+grupo2i@users.noreply.github.com>
2020-12-16 10:42:45 +0100

    Update SocialNetwork.java

--- src/java/entity/SocialNetwork.java
+++ src/java/entity/SocialNetwork.java
@@ -65,8 +65,7 @@ public class SocialNetwork implements Serializable {
             return false;
         }
         SocialNetwork other = (SocialNetwork) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
+         return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
         }
         return true;
     }
